3723c49f594c6dbc0d62e9f8032ced6047332846
Aleksandra <2023_Aleksandra.Jovanovic@iticbcn.cat>
2023-12-07 22:24:34 +0100

    subida 1

---
+++ comicFun/app/src/main/java/com/example/comicfun/lista_fondo.kt
@@ -0,0 +1,15 @@
+package com.example.comicfun
+
+import android.os.Bundle
+
+import androidx.appcompat.app.AppCompatActivity
+
+class lista_fondo : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.lista_fondo)
+    }
+
+
+}

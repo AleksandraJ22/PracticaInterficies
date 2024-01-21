cbc6eb8aac8789e178ac3ba7639248316f31eaf9
AleksandraJ22 <145792385+AleksandraJ22@users.noreply.github.com>
2024-01-21 23:16:50 +0100

    Update lista_fondo.kt

--- comicFun/app/src/main/java/com/example/comicfun/lista_fondo.kt
+++ comicFun/app/src/main/java/com/example/comicfun/lista_fondo.kt
@@ -5,12 +5,33 @@ import android.os.Bundle
 import android.view.View
 
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.comicfun.Adapter.viewHolderFondo
 
 class lista_fondo : AppCompatActivity() {
 
+
+    private lateinit var recyclerView: RecyclerView
+
+    private  lateinit var adapter: viewHolderFondo
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.lista_fondo)
+        setContentView(R.layout.lista_fondo1)
+
+
+        recyclerView = findViewById(R.id.recycleView)
+        adapter = viewHolderFondo()
+
+        recyclerView.layoutManager = LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL,false)
+
+        recyclerView.adapter=adapter
+
+
+
+
     }
 
 

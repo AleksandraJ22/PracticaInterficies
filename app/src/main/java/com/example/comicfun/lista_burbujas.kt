84ca94a2f51497ff39babcc03b8eba56e3ae637a
Aleksandra <2023_Aleksandra.Jovanovic@iticbcn.cat>
2024-01-31 19:05:51 +0100

    cambios lista_burbujas.kt

--- app/src/main/java/com/example/comicfun/lista_burbujas.kt
+++ app/src/main/java/com/example/comicfun/lista_burbujas.kt
@@ -14,12 +14,12 @@ class lista_burbujas : AppCompatActivity() {
 
     private lateinit var recyclerView: RecyclerView
 
-    private  lateinit var fondo1Adapter: AdapterFondo1
-    private lateinit var fondo1List: ArrayList<fondo>
+    private  lateinit var AdapterBurbujas: AdapterBurbujas
+    private lateinit var b_dialogo: ArrayList<fondo>
 
-    private lateinit var fondo2List: ArrayList<fondo>
+    private lateinit var b_pensamiento: ArrayList<fondo>
 
-    private lateinit var fondo3List: ArrayList<fondo>
+    private lateinit var b_grito: ArrayList<fondo>
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -37,13 +37,13 @@ class lista_burbujas : AppCompatActivity() {
         recyclerView = findViewById(R.id.recycleView)
         recyclerView.setHasFixedSize(true)
         recyclerView.layoutManager= LinearLayoutManager(this, RecyclerView.HORIZONTAL,false)
-        fondo1List = ArrayList()
+        b_dialogo = ArrayList()
 
 
         addDataToList()
 
-        fondo1Adapter=AdapterFondo1(fondo1List)
-        recyclerView.adapter = fondo1Adapter
+        AdapterBurbujas=AdapterBurbujas(b_dialogo)
+        recyclerView.adapter = AdapterBurbujas
     }
 
 
@@ -54,12 +54,12 @@ class lista_burbujas : AppCompatActivity() {
         recyclerView=findViewById(R.id.recycleView2)
         recyclerView.setHasFixedSize(true)
         recyclerView.layoutManager= LinearLayoutManager(this, RecyclerView.HORIZONTAL,false)
-        fondo2List = ArrayList()
+        b_pensamiento = ArrayList()
 
-        addDataToList2()
+        addDataToList()
 
-        fondo1Adapter=AdapterFondo1(fondo2List)
-        recyclerView.adapter = fondo1Adapter
+        AdapterBurbujas=AdapterBurbujas(b_dialogo)
+        recyclerView.adapter = AdapterBurbujas
     }
 
 
@@ -68,12 +68,13 @@ class lista_burbujas : AppCompatActivity() {
         recyclerView=findViewById(R.id.recycleView3)
         recyclerView.setHasFixedSize(true)
         recyclerView.layoutManager= LinearLayoutManager(this, RecyclerView.HORIZONTAL,false)
-        fondo3List = ArrayList()
+        b_grito = ArrayList()
 
-        addDataToList3()
-        fondo1Adapter=AdapterFondo1(fondo3List)
 
-        recyclerView.adapter = fondo1Adapter
+        addDataToList()
+
+        AdapterBurbujas=AdapterBurbujas(b_dialogo)
+        recyclerView.adapter = AdapterBurbujas
     }
 
     private fun addDataToList3(){
@@ -84,18 +85,18 @@ class lista_burbujas : AppCompatActivity() {
     private fun addDataToList2(){
 
 
-        fondo2List.add(fondo(R.drawable.grito))
-        fondo2List.add(fondo(R.drawable.grito2))
+        b_grito.add(fondo(R.drawable.grito))
+        b_grito.add(fondo(R.drawable.grito2))
 
 
     }
     private fun addDataToList(){
 
-        fondo1List.add(fondo(R.drawable.burbuja_dialogo))
-        fondo1List.add(fondo(R.drawable.burbuja_dialogo1))
-        fondo1List.add(fondo(R.drawable.dialogo3))
-        fondo1List.add(fondo(R.drawable.dialogo_4))
-        fondo1List.add(fondo(R.drawable.dialogo5))
+        b_dialogo.add(fondo(R.drawable.burbuja_dialogo))
+        b_dialogo.add(fondo(R.drawable.burbuja_dialogo1))
+        b_dialogo.add(fondo(R.drawable.dialogo3))
+        b_dialogo.add(fondo(R.drawable.dialogo_4))
+        b_dialogo.add(fondo(R.drawable.dialogo5))
 
     }
 

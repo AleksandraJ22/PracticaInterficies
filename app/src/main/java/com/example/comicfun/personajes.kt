353d6fdd2b5f81e067e29241e37e35e2292bf00a
Aleksandra <2023_Aleksandra.Jovanovic@iticbcn.cat>
2024-01-31 17:25:52 +0100

    modificaciones personajes.kt recyclerView

--- app/src/main/java/com/example/comicfun/personajes.kt
+++ app/src/main/java/com/example/comicfun/personajes.kt
@@ -4,13 +4,49 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class personajes : AppCompatActivity() {
+
+    private lateinit var recyclerView: RecyclerView
+
+    private  lateinit var fondo1Adapter: AdapterFondo1
+    private lateinit var personajes: ArrayList<fondo>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.lista_personajes)
+
+
+        init()
     }
 
+
+
+    private fun init(){
+
+        recyclerView = findViewById(R.id.recycleView)
+        recyclerView.setHasFixedSize(true)
+        recyclerView.layoutManager= LinearLayoutManager(this, RecyclerView.VERTICAL,false)
+        personajes = ArrayList()
+
+
+        addDataToList()
+
+        fondo1Adapter=AdapterFondo1(personajes)
+        recyclerView.adapter = fondo1Adapter
+    }
+
+    private fun addDataToList(){
+
+
+        personajes.add(fondo(R.drawable.personaje1))
+        personajes.add(fondo(R.drawable.personaje2))
+        personajes.add(fondo(R.drawable.perssonaje3))
+        personajes.add(fondo(R.drawable.personaje4))
+
+    }
 
     fun OkPersonaje(view:View){
 

0cc508225aa01ac90bc77847b3373f3f23e83e2b
Aleksandra <2023_Aleksandra.Jovanovic@iticbcn.cat>
2024-02-21 19:03:39 +0100

    todo

--- app/src/main/java/com/example/comicfun/BaseDatosApp.kt
+++ app/src/main/java/com/example/comicfun/BaseDatosApp.kt
@@ -4,25 +4,37 @@ import android.content.Context
 import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
 
+
 class BaseDatosApp(context: Context?, name: String?, factory: SQLiteDatabase.CursorFactory?, version:Int)
     : SQLiteOpenHelper(context, name, factory, version){
 
+    companion object {
+        private const val DATABASE_VERSION = 1
+        private const val DATABASE_NAME = "UsuariosDB"
+        private const val TABLE_NAME = "Usuarios"
+        private const val COLUMN_ID = "ID"
+        private const val COLUMN_NOMBRE = "NOMBRE"
+        private const val COLUMN_APELLIDO = "APELLIDO"
+        private const val COLUMN_PASSWORD = "PASSWORD"
+        private const val COLUMN_GMAIL = "GMAIL"
+    }
 
 
-    val create_table_users = "CREATE TABLE Usuarios" +
-            "(ID INT PRIMARY KEY," +
-            "NOMBRE TEXT," +
-            "APELLIDO TEXT," +
-            "PASSWORD TEXT," +
-            "GMAIL TEXT)"
+    private val create_table_users = "CREATE TABLE $TABLE_NAME (" +
+            "$COLUMN_ID INTEGER PRIMARY KEY AUTOINCREMENT," +
+            "$COLUMN_NOMBRE TEXT," +
+            "$COLUMN_APELLIDO TEXT," +
+            "$COLUMN_PASSWORD TEXT," +
+            "$COLUMN_GMAIL TEXT)"
+
     override fun onCreate(db: SQLiteDatabase?) {
-
         db?.execSQL(create_table_users)
-
+        //Toast.makeText(this, "Base de datos creada correctamente", Toast.LENGTH_SHORT).show()
     }
 
     override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {
-        TODO("Not yet implemented")
+        db?.execSQL("DROP TABLE IF EXISTS $TABLE_NAME")
+        onCreate(db)
     }
 
 
